a6c57bcf8250bcad60b2c1becc4a0eb58fedccfd
phalanx-poc <65502849+phalanx-poc@users.noreply.github.com>
2020-05-20 21:38:19 +0100

    Update Runner.java

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(strict = true,
 //        tags = {"@Pass"},
         features = "src/test/java",
-        plugin = {"pretty", "html:target/Report"}
+//      plugin = {"pretty", "html:target/Report"}
+        plugin = {"html:target/cucumber-pretty","json:target/cucumber.json"} 
+
 )
 public class Runner {
 
